05db451d4408b4f521f7b4d2a876965de41a9683
arcsinO <173187432+arcsinO@users.noreply.github.com>
2024-11-11 17:24:34 +0000

    accounts:login, logout

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
 
 User = get_user_model()
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "discord_id",
+        "is_staff",
+        "is_active",
+    )
+
+    search_fields = ("username", "discord_id")
+
+    ordering = ("username",)
+
+    fieldsets = (("ユーザー情報", {"fields": ("username", "discord_id", "password", "is_staff", "is_active", "date_joined")}),)
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": ("username", "discord_id", "password1", "password2"),
+            },
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,3 @@
-from django.contrib import auth
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.validators import UnicodeUsernameValidator
@@ -8,79 +7,21 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
-    use_in_migrations = True
 
-    def _create_user_object(self, username, email, password, **extra_fields):
-        if not username:
-            raise ValueError("The given username must be set")
+    def create_user(self, username, email, password=None, **extra_fields):
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
-        return user
-
-    def _create_user(self, username, email, password, **extra_fields):
-        """
-        Create and save a user with the given username, email, and password.
-        """
-        user = self._create_user_object(username, email, password, **extra_fields)
         user.save(using=self._db)
         return user
 
-    async def _acreate_user(self, username, email, password, **extra_fields):
-        """See _create_user()"""
-        user = self._create_user_object(username, email, password, **extra_fields)
-        await user.asave(using=self._db)
-        return user
-
-    def create_user(self, username, email=None, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return self._create_user(username, email, password, **extra_fields)
-
-    create_user.alters_data = True
-
-    async def acreate_user(self, username, email=None, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return await self._acreate_user(username, email, password, **extra_fields)
-
-    acreate_user.alters_data = True
-
-    def create_superuser(self, username, email=None, password=None, **extra_fields):
+    def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self._create_user(username, email, password, **extra_fields)
-
-    create_superuser.alters_data = True
-
-    async def acreate_superuser(
-        self, username, email=None, password=None, **extra_fields
-    ):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return await self._acreate_user(username, email, password, **extra_fields)
-
-    acreate_superuser.alters_data = True
+        return self.create_user(username, email, password, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    """
-    An abstract base class implementing a fully featured User model with
-    admin-compliant permissions.
-    Username and password are required. Other fields are optional.
-    """
 
     username_validator = UnicodeUsernameValidator()
 
@@ -89,31 +30,32 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         max_length=150,
         unique=True,
         help_text=_(
-            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+            "この項目は必須です。半角アルファベット、半角数字、@/./+/-/_ で150文字以下にしてください。"
         ),
         validators=[username_validator],
         error_messages={
-            "unique": _("A user with that username already exists."),
+            "unique": _("同じユーザー名が既に登録済みです。"),
         },
     )
     email = models.EmailField(_("email address"), blank=True)
+    discord_id = models.SlugField(
+        _("Discord ID"),
+        max_length=20,
+        unique=True,
+        help_text=_("DiscordのユーザーIDを本人確認メールの送信に利用します。"),
+    )
     is_staff = models.BooleanField(
         _("staff status"),
         default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
+        help_text=_("ユーザーが管理サイトにログイン可能かどうかを示します。"),
     )
     is_active = models.BooleanField(
         _("active"),
         default=True,
         help_text=_(
-            "Designates whether this user should be treated as active. "
-            "Unselect this instead of deleting accounts."
+            "ユーザーがアクティブかどうかを示します。"
+            "アカウントを削除する代わりに選択を解除してください。"
         ),
-    )
-    discord_id = models.SlugField(
-        _("discord id"),
-        max_length=20,
-        unique=True,
     )
     date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
 
