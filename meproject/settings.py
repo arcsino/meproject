381893922a65d72e04731efef53e6cc7d2cad8f7
arcsinO <173187432+arcsinO@users.noreply.github.com>
2024-11-13 19:22:08 +0000

    .env

--- meproject/settings.py
+++ meproject/settings.py
@@ -10,20 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Import .env
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r!qampkyc3+50^08zogbeim5*qt4!xhhew!a#edt#s)56m2^fw'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -77,9 +82,9 @@ WSGI_APPLICATION = 'meproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test-db',
-        'USER': 'test-user',
-        'PASSWORD': 'test-pass',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
         'HOST': 'postgres',
         'PORT': '5432',
     }
@@ -133,3 +138,6 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/accounts/index/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+# DiscordBot TOKEN
+DISCORD_BOT_TOKEN = env('DISCORD_BOT_TOKEN')
