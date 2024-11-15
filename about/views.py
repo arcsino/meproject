0d9cd6b569c28519c3b9545e050a732e5db98a69
arcsinO <173187432+arcsinO@users.noreply.github.com>
2024-11-15 16:22:27 +0000

    about

---
+++ about/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView, ListView
+from .models import Notice
+
+class IndexView(ListView):
+    """ ホームビュー """
+    template_name = "about/index.html"
+    queryset = Notice.objects.order_by("date_created")
+    context_object_name = "notice"
+    paginate_by = 10
