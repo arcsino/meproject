85e629b0d0c3b49a8ef394b981613676ad9cdbfe
arcsinO <173187432+arcsinO@users.noreply.github.com>
2024-11-17 19:31:28 +0000

    deleted

--- about/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic import TemplateView, ListView
-from .models import Notice
-
-class IndexView(ListView):
-    """ ホームビュー """
-    template_name = "about/index.html"
-    queryset = Notice.objects.order_by("-date_created")
-    context_object_name = "notice"
-    paginate_by = 10
