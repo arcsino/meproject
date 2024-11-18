bee40ce855e6fea37d042d0c272c17e8cc04d8f3
arcsinO <173187432+arcsinO@users.noreply.github.com>
2024-11-18 16:20:02 +0000

    path

--- meproject/urls.py
+++ meproject/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('notice.urls')),
     path('accounts/', include('accounts.urls')),
+    path('reminder/', include('reminder.urls')),
 ]
