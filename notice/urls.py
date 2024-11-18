e840047de435d8d765ba90e4707cc59fb799018b
arcsinO <173187432+arcsinO@users.noreply.github.com>
2024-11-18 16:20:31 +0000

    notice

--- notice/urls.py
+++ notice/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 app_name = "notice"
+
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("index/", views.IndexView.as_view(), name="index"),
--- notice/views.py
+++ notice/views.py
@@ -3,7 +3,7 @@ from .models import Notice
 
 class IndexView(ListView):
     """ ホームビュー """
-    template_name = "about/index.html"
+    template_name = "notice/index.html"
     queryset = Notice.objects.order_by("-date_created")
     context_object_name = "notice"
     paginate_by = 5
