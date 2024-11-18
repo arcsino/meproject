3eb33056fc14f220ae9bc20b3ec5758ac7a0598c
arcsinO <173187432+arcsinO@users.noreply.github.com>
2024-11-18 16:20:45 +0000

    reminder

--- reminder/views.py
+++ reminder/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from . import mixins
 
-# Create your views here.
+
+class MonthCalendar(LoginRequiredMixin, mixins.MonthCalendarMixin, generic.TemplateView):
+    """月間カレンダーを表示するビュー"""
+    template_name = 'reminder/month.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_month_calendar()
+        context.update(calendar_context)
+        return context
+
+
+class WeekCalendar(LoginRequiredMixin, mixins.WeekCalendarMixin, generic.TemplateView):
+    """週間カレンダーを表示するビュー"""
+    template_name = 'reminder/week.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_week_calendar()
+        context.update(calendar_context)
+        return context
